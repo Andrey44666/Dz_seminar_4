61fdc9ebacf346dbd93c3ddf9aea0b25ad8184c4
Oleynik Andrey <andry44666@gmail.com>
2023-02-08 17:18:42 +0300

    Complited Zadacha_1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Enter first number: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numB = int.Parse(Console.ReadLine());
+
+int Number (int numA, int numB)
+{
+    int res = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+    res *= numA;
+    }
+    return res;
+}
+System.Console.WriteLine(Number (numA,numB));
