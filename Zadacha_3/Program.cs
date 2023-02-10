75cf44ab6a5b9cb335773b8a9ac93707d3eaef11
Oleynik Andrey <andry44666@gmail.com>
2023-02-11 01:39:58 +0300

    Complited Zadach_3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] nums = new int[8];
+
+for (int i = 0; i<8; i++) {
+    Console.Write("Enter number " +  (i + 1) + ": ");
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+}
+Console.Write("[");
+
+for (int i = 0; i<nums.Length; i++) {
+    Console.Write(nums[i] + ", ");
+}
+Console.Write("]");
